3a2d9e2416badfbbbd3a5c618d74ce989eacf940
Ayush Thakur <workayush2006@gmail.com>
2025-04-05 18:17:20 +0530

    end of ui design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import { Link } from 'react-scroll';
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 const Navbar = () => {
@@ -16,14 +16,15 @@ window.addEventListener("scroll",()=>{window.scrollY > window.innerHeight*.8 ? s
 
   return (
 <nav className={`navbar ${sticky? 'dark-nav':''}`}>
-    <img src={logo} className="logo"/>
+    <img src={logo} className="logo"/>HomeProgramAbout usCampusTestimonial￼Contact us
+
     <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonial</li>
-        <li><button className='btn'>Contact us</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-110} duration={500}>About us</Link></li>
+        <li><Link to='campus' smooth={true} offset={-300} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-300} duration={500}>Testimonial</Link></li>
+        <li><button className='btn'><Link to='contact' smooth={true} offset={0} duration={500}>Contact us</Link></button></li>
     </ul>
 </nav>
   )
